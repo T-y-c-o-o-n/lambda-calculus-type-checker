74db4ffeb905e367c9786765e09720d20b72feba
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-02-11 00:25:18 +0300

    rewritten Show instances for Type and Term

--- src/Base.hs
+++ src/Base.hs
@@ -13,10 +13,20 @@ infixr 4 :=>
 data Type
   = T TypeVar
   | Type :=> Type
-  deriving (Ord, Eq, Show)
+  deriving (Ord, Eq)
+
+instance Show Type where
+  show (T a) = a
+  show (t1@(T _) :=> t2) = show t1 ++ " -> " ++ show t2
+  show (t1 :=> t2) = "(" ++ show t1 ++ ") -> " ++ show t2
 
 data Term
   = V Var
   | Term :@ Term
   | L Var Type Term
-  deriving (Ord, Eq, Show)
+  deriving (Ord, Eq)
+
+instance Show Term where
+  show (V x) = x
+  show (t1 :@ t2) = show t1 ++ " " ++ show t2
+  show (L x t term) = "\\" ++ x ++ ": " ++ show t ++ ". " ++ show term
