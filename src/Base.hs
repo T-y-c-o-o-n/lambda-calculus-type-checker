8205a79dcad479e79d08959d02903072422b6935
Nikita Siniachenko <ntpmsoakk070601@gmail.com>
2022-02-10 16:19:47 +0300

    added Base with lambda calculus types and terms

---
+++ src/Base.hs
@@ -0,0 +1,19 @@
+module Base where
+
+type Var = String
+
+type TypeVar = String
+
+infixl 4 :@
+
+data Type
+  = T TypeVar
+  | Type :=> Type
+  deriving (Ord, Eq)
+
+data Term
+  = V Var
+  | Term :@ Term
+  | Term :@: Type
+  | L Var Type Term
+  deriving (Ord, Eq)
